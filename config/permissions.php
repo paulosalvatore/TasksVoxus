2795e08cc9ee7f67c83d1cd50fdbbc67218cc79b
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-12-07 15:44:32 -0200

    Config/permissions criado

---
+++ config/permissions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulo
+ * Date: 07/12/2017
+ * Time: 15:43
+ */
+
+return [
+	"Users.SimpleRbac.permissions" => [
+		[
+			"role" => "user",
+			"controller" => "Tasks",
+			"action" => "index",
+		]
+	]];
