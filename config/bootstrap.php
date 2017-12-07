8bb0bf2dfa5250d02bb79dfa950e4bacf1156806
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-12-07 15:37:59 -0200

    Dependencias atualizadas

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -16,7 +16,7 @@
 /*
  * Configure paths required to find CakePHP + general filepath constants
  */
-require __DIR__ . '/paths.php';
+require __DIR__ . "/paths.php";
 
 /*
  * Bootstrap CakePHP.
@@ -27,7 +27,7 @@ require __DIR__ . '/paths.php';
  * - Registering the CakePHP autoloader.
  * - Setting the default application paths.
  */
-require CORE_PATH . 'config' . DS . 'bootstrap.php';
+require CORE_PATH . "config" . DS . "bootstrap.php";
 
 use Cake\Cache\Cache;
 use Cake\Console\ConsoleErrorHandler;
@@ -49,8 +49,8 @@ use Cake\Utility\Security;
  * You should copy `config/.env.default to `config/.env` and set/modify the
  * variables as required.
  */
-// if (!env('APP_NAME') && file_exists(CONFIG . '.env')) {
-//     $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . '.env']);
+// if (!env("APP_NAME") && file_exists(CONFIG . ".env")) {
+//     $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . ".env"]);
 //     $dotenv->parse()
 //         ->putenv()
 //         ->toEnv()
@@ -66,8 +66,8 @@ use Cake\Utility\Security;
  * that changes from configuration that does not. This makes deployment simpler.
  */
 try {
-    Configure::config('default', new PhpConfig());
-    Configure::load('app', 'default', false);
+    Configure::config("default", new PhpConfig());
+    Configure::load("app", "default", false);
 } catch (\Exception $e) {
     exit($e->getMessage() . "\n");
 }
@@ -77,15 +77,15 @@ try {
  * You can use a file like app_local.php to provide local overrides to your
  * shared configuration.
  */
-//Configure::load('app_local', 'default');
+//Configure::load("app_local", "default");
 
 /*
  * When debug = true the metadata cache should only last
  * for a short time.
  */
-if (Configure::read('debug')) {
-    Configure::write('Cache._cake_model_.duration', '+2 minutes');
-    Configure::write('Cache._cake_core_.duration', '+2 minutes');
+if (Configure::read("debug")) {
+    Configure::write("Cache._cake_model_.duration", "+2 minutes");
+    Configure::write("Cache._cake_core_.duration", "+2 minutes");
 }
 
 /*
@@ -93,34 +93,34 @@ if (Configure::read('debug')) {
  * choice but using UTC makes time calculations / conversions easier.
  * Check http://php.net/manual/en/timezones.php for list of valid timezone strings.
  */
-date_default_timezone_set('UTC');
+date_default_timezone_set("UTC");
 
 /*
  * Configure the mbstring extension to use the correct encoding.
  */
-mb_internal_encoding(Configure::read('App.encoding'));
+mb_internal_encoding(Configure::read("App.encoding"));
 
 /*
  * Set the default locale. This controls how dates, number and currency is
  * formatted and sets the default language to use for translations.
  */
-ini_set('intl.default_locale', Configure::read('App.defaultLocale'));
+ini_set("intl.default_locale", Configure::read("App.defaultLocale"));
 
 /*
  * Register application error and exception handlers.
  */
-$isCli = PHP_SAPI === 'cli';
+$isCli = PHP_SAPI === "cli";
 if ($isCli) {
-    (new ConsoleErrorHandler(Configure::read('Error')))->register();
+    (new ConsoleErrorHandler(Configure::read("Error")))->register();
 } else {
-    (new ErrorHandler(Configure::read('Error')))->register();
+    (new ErrorHandler(Configure::read("Error")))->register();
 }
 
 /*
  * Include the CLI bootstrap overrides.
  */
 if ($isCli) {
-    require __DIR__ . '/bootstrap_cli.php';
+    require __DIR__ . "/bootstrap_cli.php";
 }
 
 /*
@@ -129,25 +129,25 @@ if ($isCli) {
  *
  * If you define fullBaseUrl in your config file you can remove this.
  */
-if (!Configure::read('App.fullBaseUrl')) {
+if (!Configure::read("App.fullBaseUrl")) {
     $s = null;
-    if (env('HTTPS')) {
-        $s = 's';
+    if (env("HTTPS")) {
+        $s = "s";
     }
 
-    $httpHost = env('HTTP_HOST');
+    $httpHost = env("HTTP_HOST");
     if (isset($httpHost)) {
-        Configure::write('App.fullBaseUrl', 'http' . $s . '://' . $httpHost);
+        Configure::write("App.fullBaseUrl", "http" . $s . "://" . $httpHost);
     }
     unset($httpHost, $s);
 }
 
-Cache::setConfig(Configure::consume('Cache'));
-ConnectionManager::setConfig(Configure::consume('Datasources'));
-Email::setConfigTransport(Configure::consume('EmailTransport'));
-Email::setConfig(Configure::consume('Email'));
-Log::setConfig(Configure::consume('Log'));
-Security::setSalt(Configure::consume('Security.salt'));
+Cache::setConfig(Configure::consume("Cache"));
+ConnectionManager::setConfig(Configure::consume("Datasources"));
+Email::setConfigTransport(Configure::consume("EmailTransport"));
+Email::setConfig(Configure::consume("Email"));
+Log::setConfig(Configure::consume("Log"));
+Security::setSalt(Configure::consume("Security.salt"));
 
 /*
  * The default crypto extension in 3.0 is OpenSSL.
@@ -159,12 +159,12 @@ Security::setSalt(Configure::consume('Security.salt'));
 /*
  * Setup detectors for mobile and tablet.
  */
-ServerRequest::addDetector('mobile', function ($request) {
+ServerRequest::addDetector("mobile", function ($request) {
     $detector = new \Detection\MobileDetect();
 
     return $detector->isMobile();
 });
-ServerRequest::addDetector('tablet', function ($request) {
+ServerRequest::addDetector("tablet", function ($request) {
     $detector = new \Detection\MobileDetect();
 
     return $detector->isTablet();
@@ -178,13 +178,13 @@ ServerRequest::addDetector('tablet', function ($request) {
  * locale specific date formats. For details see
  * @link https://book.cakephp.org/3.0/en/core-libraries/internationalization-and-localization.html#parsing-localized-datetime-data
  */
-Type::build('time')
+Type::build("time")
     ->useImmutable();
-Type::build('date')
+Type::build("date")
     ->useImmutable();
-Type::build('datetime')
+Type::build("datetime")
     ->useImmutable();
-Type::build('timestamp')
+Type::build("timestamp")
     ->useImmutable();
 
 /*
@@ -192,10 +192,10 @@ Type::build('timestamp')
  * table, model, controller names or whatever other string is passed to the
  * inflection functions.
  */
-//Inflector::rules('plural', ['/^(inflect)or$/i' => '\1ables']);
-//Inflector::rules('irregular', ['red' => 'redlings']);
-//Inflector::rules('uninflected', ['dontinflectme']);
-//Inflector::rules('transliteration', ['/å/' => 'aa']);
+//Inflector::rules("plural", ["/^(inflect)or$/i" => "\1ables"]);
+//Inflector::rules("irregular", ["red" => "redlings"]);
+//Inflector::rules("uninflected", ["dontinflectme"]);
+//Inflector::rules("transliteration", ["/å/" => "aa"]);
 
 /*
  * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
@@ -203,7 +203,7 @@ Type::build('timestamp')
  * advanced ways of loading plugins
  *
  * Plugin::loadAll(); // Loads all plugins at once
- * Plugin::load('Migrations'); //Loads a single plugin named Migrations
+ * Plugin::load("Migrations"); //Loads a single plugin named Migrations
  *
  */
 
@@ -211,6 +211,9 @@ Type::build('timestamp')
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
  */
-if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
+if (Configure::read("debug")) {
+    Plugin::load("DebugKit", ["bootstrap" => true]);
 }
+
+Configure::write("Users.config", ["users"]);
+Plugin::load("CakeDC/Users", ["routes" => true, "bootstrap" => true]);
