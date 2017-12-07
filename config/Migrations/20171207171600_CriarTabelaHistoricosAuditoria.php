4ec484d36e2459a1899a9a80bd6e2b2bf09eaff3
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-12-07 17:28:12 -0200

    Revisão de Tabelas

--- config/Migrations/20171207171600_CriarTabelaHistoricosAuditoria.php
+++ config/Migrations/20171207171600_CriarTabelaHistoricosAuditoria.php
@@ -7,12 +7,12 @@ class CriarTabelaHistoricosAuditoria extends AbstractMigration
 	{
 		$table = $this->table("historicos_auditoria");
 		$table->addColumn("model", "string")
-			->addColumn("foreign_key", "integer", ["null" => true])
+			->addColumn("foreign_key", "integer")
 			->addColumn("campo", "string")
 			->addColumn("anterior", "text", ["null" => true])
 			->addColumn("novo", "text", ["null" => true])
-			->addColumn("user_id", "char", ["limit" => 36, "null" => true])
-			->addForeignKey("user_id", "users", "id")
+			->addColumn("usuario_id", "integer")
+			->addForeignKey("usuario_id", "usuarios", "id")
 			->addColumn("criado", "datetime")
 			->save();
 	}
